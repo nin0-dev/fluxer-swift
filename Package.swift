4abacae6630579d98e15dbd34d205a691b9efaed
nin0 <personal@nin0.dev>
2026-02-21 17:09:33 -0500

    init

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 6.2
+
+import PackageDescription
+
+let package = Package(
+    name: "fluxer-swift",
+    platforms: [
+        .macOS(.v10_15)
+    ],
+    products: [
+        .library(
+            name: "FluxerSwift",
+            targets: ["FluxerSwift"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.16.1")
+    ],
+    targets: [
+        .target(
+            name: "FluxerSwift",
+            dependencies: [
+                .product(name: "WebSocketKit", package: "websocket-kit")
+            ]
+        ),
+        .executableTarget(
+            name: "Example",
+            dependencies: ["FluxerSwift"]
+        ),
+    ]
+)
---
+++ Sources/Example/main.swift
@@ -0,0 +1,3 @@
+import FluxerSwift
+
+test()
